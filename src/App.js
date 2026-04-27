5f37f392e096e5fae0393badecea979f7af37978
Dipannita Biswas <dipannitabiswas.pue@gmailcom>
2026-04-27 17:33:49 +0530

    feat: feature added in imagecard component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Imagecard from './Components/Imagecard'; // imagecard is a functional com
 import './App.css';
 import {userData} from './constants/constant'; // we can import any variable or function from other file by using export and import keyword. normal export
 function App() {
-  const [showFlag, setShowFlag] = useState(false); // useState is a hook which is used to manage state in functional component. it returns an array of two elements. first element is the state variable and second element is the function which is used to update the state variable. we can use array destructuring to get the state variable and the function.
-  const clickHandler = () => {
-    showFlag ? setShowFlag(false) : setShowFlag(true);
+  const [showFlag, setShowFlag] = useState(null); // useState is a hook which is used to manage state in functional component. it returns an array of two elements. first element is the state variable and second element is the function which is used to update the state variable. we can use array destructuring to get the state variable and the function.
+  const clickHandler = (id) => {
+    showFlag ? setShowFlag(null) : setShowFlag(id);
   };
   return (
     //JSX= javascript xml
@@ -14,13 +14,15 @@ function App() {
       userData.map((user) => (
         <div key={user.ImgLink}>  
         <Imagecard Name={user.Name} ImgLink={`https://avatars.githubusercontent.com/u/${user.ImgLink}?v=4`} />
-        <h3 style={{ cursor: 'pointer' }} onClick={clickHandler}>{user.isStudent ? "Student" : "Learner"} ⬇️⬇️⬇️
+        <h3 style={{ cursor: 'pointer' }} onClick={()=>clickHandler(user.ImgLink)}>
+          {user.isStudent ? "Student" : "Learner"} ⬇️⬇️⬇️
         </h3>
-        {showFlag ? <p>{user.biodescription}</p> : null}
+        {showFlag === user.ImgLink  && ( <p>{user.biodescription}</p>)}
         </div>
       ))
     }
     </div>
+
   );
 }
 //always pass key in map funtion.
